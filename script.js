f7d1c891dc4c5e2ff7d7626ff8e4c5fe914030d6
eric-chambonneau <101542272+eric-chambonneau@users.noreply.github.com>
2023-02-27 21:31:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,29 +2,67 @@
 var sp, btn_start, btn_stop, t, ms, s, mn, h;
 //la fonction pourinitialiser les variables quand la page se charge
 window.onload = function(){
-    sp = document.getElementsByTagName("sp");
-    btn_start = document.getElementById("btn_start");
-    btn_stop = document.getElementById("btn_stop");
-    t = 0;
-    ms = 0;
-    s = 0;
-    mn = 0;
-    h = 0;
+    sp = document.getElementsByTagName("span");
+    btn_start = document.getElementById("start");
+    btn_stop = document.getElementById("stop");
+    t;
+    ms=0;
+    s=0;
+    mn=0;
+    h=0;
    
 }
 //mettre en place le compteur
 function update_chrono(){
     ms+=1;
     if(ms == 10){
-        ms = 0;
+        ms=1;
         s+=1;
     }
-    if(s == 60){
-        s = 0;
+    if(s==60){
+        s=0;
         mn+=1;
     }
-    if(mn = 60){
-        mn = 0;
-        
+    if(mn==60){
+        mn=0;
+        h+=1;
     }
+    //insertion des valeurs dans le span
+    //[0] pour de sélectionner le premier span
+    //[1] pour de sélectionner le deuxième span etc...
+
+    sp[0].innerHTML = h + "h";
+    sp[1].innerHTML = mn + "min";
+    sp[2].innerHTML = s + "s";
+    sp[3].innerHTML = ms + "ms";
+
+}
+
+//mettre en place la fonction le bouton start
+
+function start(){
+    //cette ligne permet de lancer la fonction update_chrono toutes les 100ms
+    t =setInterval(update_chrono,100);
+    btn_start.disabled = true;
+    
+}
+//stopper le chrono
+function stop(){
+    clearInterval(t);//suppression de l'interval
+    btn_start.disabled = false;
+}
+
+//réinitialiser le chrono
+function reset(){
+    clearInterval(t);
+    btn_start.disabled = false;
+    
+    ms = 0;
+    s = 0;
+    mn = 0;
+    h = 0;
+    p[0].innerHTML = h + "h";
+    sp[1].innerHTML = mn + "min";
+    sp[2].innerHTML = s + "s";
+    sp[3].innerHTML = ms + "ms";
 }
